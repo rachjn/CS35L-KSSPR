db207d156cfba11b11fb417f3817c440c1d15351
King0508 <king0508@g.ucla.edu>
2024-10-26 18:35:39 -0700

    added game page and profile page
    clicking region squares now are linked to the game page. A profile icon is added to the top right of the home page.

---
+++ client/app/game/page.js
@@ -0,0 +1,10 @@
+import { PageShell } from "@/components/PageShell";
+import GameInterface from "./GameInterface";
+
+export default function Game() {
+    return (
+        <PageShell title="Game">
+            <GameInterface />
+        </PageShell>
+    );
+}
