a7c8e2b1edf0c88e657a1ee8fa0755574a220f62
Sachin Raja <me@s4n.land>
2024-10-26 20:47:32 -0700

    format

--- client/app/game/page.js
+++ client/app/game/page.js
@@ -2,9 +2,9 @@ import { PageShell } from "@/components/PageShell";
 import GameInterface from "./GameInterface";
 
 export default function Game() {
-    return (
-        <PageShell title="Game">
-            <GameInterface />
-        </PageShell>
-    );
+  return (
+    <PageShell title="Game">
+      <GameInterface />
+    </PageShell>
+  );
 }
