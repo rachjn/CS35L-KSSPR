99249515c0083af9071c4564786138fa983a3118
Sachin Raja <me@s4n.land>
2024-11-06 19:26:19 -0800

    add live feedback

--- client/app/game/GameInterface.js
+++ client/app/game/GameInterface.js
@@ -5,11 +5,29 @@ import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
+function getErrorOutput(transcript, input) {
+  const transcriptWords = transcript.split(" ");
+  const inputWords = input.split(" ");
+
+  const output = [];
+
+  for (let i = 0; i < inputWords.length - 1; i++) {
+    if (inputWords[i] !== transcriptWords[i]) {
+      output.push({ word: inputWords[i], error: true });
+    } else {
+      output.push({ word: inputWords[i], error: false });
+    }
+  }
+
+  return output;
+}
+
 export default function GameInterface() {
   const searchParams = useSearchParams();
   const region = searchParams.get("region");
-
+  const [input, setInput] = useState("");
   const [challenge, setChallenge] = useState(null);
+  const [errorOutput, setErrorOutput] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:3001/api/challenges/region?region=" + region)
@@ -47,13 +65,30 @@ export default function GameInterface() {
       <div className="flex flex-col gap-4">
         {/* Area where words show up if we are doing it */}
         <div className="min-h-20 border border-black p-4 bg-gray-100">
-          {challenge && <Text className="text-lg">{challenge.transcript}</Text>}
+          {errorOutput.map(({ word, error }, index) =>
+            error ? (
+              <span key={index} className="text-lg text-red-500">
+                {word}{" "}
+              </span>
+            ) : (
+              <span key={index} className="text-lg">
+                {word}{" "}
+              </span>
+            ),
+          )}
         </div>
 
         {/* User input field */}
         <input
           type="text"
           placeholder="Type words here..."
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+            setErrorOutput(
+              getErrorOutput(challenge.transcript, e.target.value),
+            );
+          }}
           className="w-full p-4 border border-black text-lg focus:outline-none"
         />
       </div>
