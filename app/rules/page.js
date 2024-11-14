176a9a286d24d6bd409a4ed0b4d12817c316af6f
Sachin Raja <me@s4n.land>
2024-11-13 16:53:44 -0800

    format

--- app/rules/page.js
+++ app/rules/page.js
@@ -1,8 +1,5 @@
 import { PageShell } from "@/components/PageShell";
 
 export default function Rules() {
-    return (
-      <PageShell title="Rules">
-        Rules </PageShell>
-    )
+  return <PageShell title="Rules">Rules </PageShell>;
 }
