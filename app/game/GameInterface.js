6661516bfb66fc53d914cee8d24f583842c2d361
Rachel Jin <racheljin24@gmail.com>
2024-11-27 10:41:27 -0800

    game interface updated

--- app/game/GameInterface.js
+++ app/game/GameInterface.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { HomeButton } from "@/components/HomeButton";
 import { Text } from "@/components/Text";
 import { getRandomChallengeByRegion } from "@/lib/actions/get-challenge";
 import { recordScore } from "@/lib/actions/get-scores";
@@ -84,81 +85,76 @@ export default function GameInterface() {
     .join(" ");
 
   return (
-    <div className="flex flex-col gap-6 w-full max-w-2xl mx-auto">
-      {/* Region Title */}
-      <div className="border border-black p-4 bg-white">
-        <Text className="text-2xl">
-          {displayRegion}: {challenge && challenge.title}
-        </Text>
-      </div>
-
-      {challenge && <audio controls src={challenge.audioURL} />}
-
-      {/* Score Block */}
-      {isGameOver && (
-        <div className="border border-black p-4 mt-4">
-          <Text className="text-lg font-bold">Game Over</Text>
-          <div className="flex justify-between">
-            <Text>
-              Score: {score} - {wpm} WPM
-            </Text>
+    <>
+      <HomeButton />
+      <div className="flex justify-center items-center h-screen">
+        <div className="bg-dark-brown p-12 rounded-lg flex flex-col items-center gap-6">
+          {/* Timer Block */}
+          <div className="rounded-full p-4 bg-my-blue border border-white border-opacity-40 shadow">
+            <div className="text-xl font-bold text-dark-brown">
+              {timerSeconds} seconds left
+            </div>
           </div>
-          {/* Button to navigate to Scoreboard */}
-          <Link href="/scoreboard">
-            <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded">
-              View Scoreboard
-            </button>
-          </Link>
-        </div>
-      )}
 
-      {/* Timer Block */}
-      <div className="border border-black p-4 bg-white">
-        <Text className="text-xl">{timerSeconds} seconds left</Text>
-      </div>
+          {/* Region Title */}
+          <div className="text-5xl font-bold text-lighter-brown lowercase ">
+            {displayRegion}: {challenge && challenge.title}
+          </div>
 
-      {/* Text Input Area */}
-      <div className="flex flex-col gap-4">
-        {/* Area where words show up if we are doing it */}
-        <div className="min-h-20 border border-black p-4 bg-gray-100">
-          {errorOutput.map(({ word, error }, index) =>
-            error ? (
-              <span key={index} className="text-lg text-red-500">
-                {word}{" "}
-              </span>
-            ) : (
-              <span key={index} className="text-lg text-green-500">
-                {word}{" "}
-              </span>
-            ),
+          {challenge && <audio controls src={challenge.audioURL} />}
+
+          {/* Score Block */}
+          {isGameOver && (
+            <div className="flex items-center justify-center flex-col text-dark-brown bg-light-beige py-4 px-12 rounded">
+              <div className="font-bold text-4xl">game over!</div>
+              <div className="text-base lowercase">
+                Score: {score} - {wpm} WPM
+              </div>
+              {/* Button to navigate to Scoreboard */}
+              <Link href="/scoreboard">
+                <button className="mt-4 bg-my-pink text-dark-brown py-2 px-8 font-bold border border-white border-opacity-40 shadow rounded lowercase">
+                  View Scoreboard
+                </button>
+              </Link>
+            </div>
           )}
+
+          {/* Text Input Area */}
+          <div className="flex flex-col gap-4 min-w-[40rem]  ">
+            {/* Area where words show up if we are doing it */}
+            <div className="min-h-20 border border-light-brown p-4 bg-lighter-brown rounded min-h-[10rem]">
+              {errorOutput.map(({ word, error }, index) =>
+                error ? (
+                  <span key={index} className="text-lg text-red-500">
+                    {word}{" "}
+                  </span>
+                ) : (
+                  <span key={index} className="text-lg text-dark-brown">
+                    {word}{" "}
+                  </span>
+                ),
+              )}
+            </div>
+
+            {/* User input field */}
+            <input
+              type="text"
+              placeholder="Type words here..."
+              value={input}
+              disabled={isGameOver}
+              onChange={(e) => {
+                startTransition(() => {
+                  setInput(e.target.value);
+                  setErrorOutput(
+                    getErrorOutput(challenge.transcript, e.target.value),
+                  );
+                });
+              }}
+              className="w-full p-4 border border-lighter-brown rounded-lg text-dark-brown text-lg focus:outline-none"
+            />
+          </div>
         </div>
-
-        {/* User input field */}
-        <input
-          type="text"
-          placeholder="Type words here..."
-          value={input}
-          disabled={isGameOver}
-          onChange={(e) => {
-            startTransition(() => {
-              setInput(e.target.value);
-              setErrorOutput(
-                getErrorOutput(challenge.transcript, e.target.value),
-              );
-            });
-          }}
-          className="w-full p-4 border border-black text-lg focus:outline-none"
-        />
       </div>
-
-      {/* Back Button */}
-      <Link
-        href="/region"
-        className="bg-gray-600 py-1 px-8 border border-black self-start"
-      >
-        <Text className="text-xl">Back</Text>
-      </Link>
-    </div>
+    </>
   );
 }
