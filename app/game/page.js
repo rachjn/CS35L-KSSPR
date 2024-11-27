8befe70e4d90b45cccd62970606179723262b441
Rachel Jin <racheljin24@gmail.com>
2024-11-27 10:41:27 -0800

    game interface updated

--- app/game/page.js
+++ app/game/page.js
@@ -2,9 +2,5 @@ import { PageShell } from "@/components/PageShell";
 import GameInterface from "./GameInterface";
 
 export default function Game() {
-  return (
-    <PageShell title="Game">
-      <GameInterface />
-    </PageShell>
-  );
+  return <GameInterface />;
 }
